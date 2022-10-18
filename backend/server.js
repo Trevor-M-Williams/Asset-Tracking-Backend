bf4fe4ba370c2242e05823830b685317dea4a930
Trevor Williams <tmw7991@gmail.com>
2022-10-18 16:49:43 -0600

    server updates

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res) => {
-  res.send("Hello World");
-});
 app.use("/api/locations", require("./routes/locationRoutes"));
 
 app.use(errorHandler);
